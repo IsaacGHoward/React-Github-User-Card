c0443e520a606368f0ab35087bca36f6ce2c1b58
IsaacGHoward <howardisaac100@gmail.com>
2020-12-05 15:40:09 -0800

    Create-react-app

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <h1>Hello</h1>
+        
+    );
+  }
+}
+
+export default App;
