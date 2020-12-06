f80f1dca42e1c6bd6cfa879894e18a90987025f2
IsaacGHoward <howardisaac100@gmail.com>
2020-12-05 16:21:58 -0800

    All users now show up

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -5,14 +5,25 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { users: []};
+    this.state = { users: [], username:'IsaacGHoward'};
   }
   componentDidMount(){
-    axios.get("https://api.github.com/users/IsaacGHoward")
+    axios.get(`https://api.github.com/users/${this.state.username}`)
     .then(res => {
       console.log(res);
-      this.setState({users: [...this.state.users, {img: res.data.avatar_url, name: res.data.name, username: res.data.login, location: res.data.location, url: res.data.html_url, followers: res.data.followers, following: res.data.following, bio: res.data.bio }]})
+      this.setState({users: [...this.state.users, {img: res.data.avatar_url, name: res.data.name, username: res.data.login, location: res.data.location, url: res.data.html_url, followers: res.data.followers, following: res.data.following, bio: res.data.bio }]});
     })
+    .then(
+      axios.get(`https://api.github.com/users/${this.state.username}/following`)
+      .then(res => {
+        for( let user in res.data){
+          axios.get(`https://api.github.com/users/${res.data[user].login}`)
+          .then(res => {
+            this.setState({users: [...this.state.users, {img: res.data.avatar_url, name: res.data.name, username: res.data.login, location: res.data.location, url: res.data.html_url, followers: res.data.followers, following: res.data.following, bio: res.data.bio }]});
+          })
+        }
+      })
+    )
   }
   render() {
     return (
