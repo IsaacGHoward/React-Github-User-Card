89d234b989a49da43b11abd17c757fe76b115a4d
IsaacGHoward <howardisaac100@gmail.com>
2020-12-05 16:08:09 -0800

    Got my usercard showing up

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
 import UserCard from './UserCard';
+import axios from 'axios';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = { users: []};
+  }
+  componentDidMount(){
+    axios.get("https://api.github.com/users/IsaacGHoward")
+    .then(res => {
+      console.log(res);
+      this.setState({users: [...this.state.users, {img: res.data.avatar_url, name: res.data.name, username: res.data.login, location: res.data.location, url: res.data.html_url, followers: res.data.followers, following: res.data.following, bio: res.data.bio }]})
+    })
   }
   render() {
     return (
       <div className="container">
-        <UserCard/>
+        {
+          this.state.users.map( user => {
+            return <UserCard userdata={user}/>
+          })
+        }
       </div>
       
         
--- react-github-user-card/src/UserCard.js
+++ react-github-user-card/src/UserCard.js
@@ -9,17 +9,17 @@ class UserCard extends React.Component {
     render() {
       return (
         <div className="card">
-            <img src={this.props.img} alt="User PFP"/>
+            <img src={this.props.userdata.img} alt="User PFP"/>
             <div className="card-info">
-                <h3 className="name">{this.props.name}</h3>
-                <p className="username">{this.props.username}</p>
-                    <p>Location: {this.props.location}</p>
+                <h3 className="name">{this.props.userdata.name}</h3>
+                <p className="username">{this.props.userdata.username}</p>
+                    <p>Location: {this.props.userdata.location}</p>
                     <p>Profile:
-                    <a href={this.props.url}>{this.props.url}</a>
+                    <a href={this.props.userdata.url}>{this.props.userdata.url}</a>
                 </p>
-                <p>Followers: {this.props.followers}</p>
-                <p>Following: {this.props.following}</p>
-                <p>Bio: {this.props.bio}</p>
+                <p>Followers: {this.props.userdata.followers}</p>
+                <p>Following: {this.props.userdata.following}</p>
+                <p>Bio: {this.props.userdata.bio}</p>
             </div>
         </div>
       );
